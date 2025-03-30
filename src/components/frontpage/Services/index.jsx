e99cc6e3944c521970f721e8b281daaf691e788d
Igor J.L. Ndiramiye <nigorjeanluc@gmail.com>
2025-03-30 18:32:35 +0200

    New Changes 3

--- src/components/frontpage/Services/index.jsx
+++ src/components/frontpage/Services/index.jsx
@@ -171,6 +171,8 @@ function Services() {
             <MotionBox
               direction="column"
               w={{ base: "100%", md: "30%" }}
+              paddingX={{ base: "4", md: "0" }}
+              justify={{ base: "center", md: "flex-start" }}
               initial={{ opacity: 0, x: -50 }}
               whileInView={{ opacity: 1, x: 0 }}
               transition={{ duration: 0.8 }}
